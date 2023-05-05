971e320dc7612c0611966cbb8774c26918f8e379
Arzu Caner <arzuguneycaner@gmail.com>
2023-05-05 23:43:28 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const tweetsData = [
+    {
+        handle: `@TrollBot66756542`,
+        profilePicture: `images/software.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `In general, important topics in a software architecture are OOP, Design patterns, Separation of concerns, Scalability, Security, 
+         and Performance`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+       
+    },
+]
