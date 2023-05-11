c0bc6b95b992bc9044322e7ed38f32c749a6e711
Arzu Caner <arzuguneycaner@gmail.com>
2023-05-11 22:58:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,36 @@ tweetBtn.addEventListener('click', function() {
     console.log(tweetInput.value)
 
 })
+
+function getFeedHtml(){
+
+    let feedHtml = ``
+    tweetsData.forEach(function (tweet){
+        feedHtml += `
+    <div class="tweet">
+       <div class="tweet-inner">
+       <img src="${tweet.profilePicture}" class="profile-picture>
+       <div>
+           <p class="handle">${tweet.handle}</p>
+           <p class="tweet-text">${tweet.tweetText}</p>
+           <div class="tweet-details">
+                <span class="tweet-detail">
+                      ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                   ${tweet.retweets}
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+`
+        
+    })
+    return feedHtml
+}
+
+console.log(getFeedHtml)
