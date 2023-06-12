43bccf65d2e8ea49f80a43fb908b57c95ebae512
Arzu Caner <arzuguneycaner@gmail.com>
2023-06-12 22:18:57 +0100

    feat: Add test case to verify correct number of tweets are displayed

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,7 +3,6 @@ import { tweetsData } from './data.js';
 import { render, handleLikeClick, handleRetweetClick, handleReplyClick } from './index.js';
 
 test('handleLikeClick should toggle the like status and update the likes count', () => {
-
     const tweetId = tweetsData[0].uuid;
     const initialLikeCount = tweetsData[0].likes;
     const likeButton = document.createElement('button');
@@ -14,7 +13,6 @@ test('handleLikeClick should toggle the like status and update the likes count',
     expect(tweetsData[0].isLiked).toBe(true);
     expect(tweetsData[0].likes).toBe(initialLikeCount + 1);
 
-
     handleLikeClick(tweetId);
 
     expect(tweetsData[0].isLiked).toBe(false);
@@ -22,7 +20,6 @@ test('handleLikeClick should toggle the like status and update the likes count',
 });
 
 test('handleRetweetClick should toggle the retweet status and update the retweets count', () => {
-
     const tweetId = tweetsData[0].uuid;
     const initialRetweetCount = tweetsData[0].retweets;
     const retweetButton = document.createElement('button');
@@ -40,14 +37,12 @@ test('handleRetweetClick should toggle the retweet status and update the retweet
 });
 
 test('handleReplyClick should toggle the visibility of the reply section', () => {
-
     const replyId = tweetsData[0].uuid;
     const replyButton = document.createElement('button');
     replyButton.dataset.reply = replyId;
     const replySection = document.createElement('div');
     replySection.id = `replies-${replyId}`;
     replySection.classList.add('tweet-replies', 'hidden');
-
 
     handleReplyClick(replyId);
 
@@ -58,3 +53,12 @@ test('handleReplyClick should toggle the visibility of the reply section', () =>
     expect(replySection.classList.contains('hidden')).toBe(true);
 });
 
+test('render should display the correct number of tweets', () => {
+    const container = document.createElement('div');
+    const tweetsCount = tweetsData.length;
+
+    render(container);
+
+    const renderedTweets = container.querySelectorAll('.tweet');
+    expect(renderedTweets.length).toBe(tweetsCount);
+});
